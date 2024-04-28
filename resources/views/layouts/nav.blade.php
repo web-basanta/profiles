f6f967da37e4671edd8d7b1891799c5b17847466
basanta <basanta.solivatech@gmail.com>
2024-04-28 22:27:50 +0530

    nav logout section change

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -176,10 +176,16 @@
             Activity Log
           </a>
           <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="{{ route('logout') }}">
-            <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-            Logout
+          <a class="dropdown-item" href="{{ route('logout') }}"
+              onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+                            <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+              {{ __('Logout') }}
           </a>
+
+          <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+              @csrf
+          </form>
         </div>
       </li>
     
