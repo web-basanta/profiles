0e1708c25dfb0bc23685f8b2488b5ec82ac6cea8
BASANTA MANNA <110733516+Basantaweb@users.noreply.github.com>
2024-05-07 07:03:40 +0000

    for route test

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -4,8 +4,25 @@
   
 @section('content')
 
-    <div class="row">
-        <!-- <a href="{{ route('games.ticktak') }}" class="btn btn-success p-1">Tacktic tac toe</a> -->
+<div class="container-fluid">
+  <div class="row">
+    Dashboard
+  </div>
+  <div class="row">
+
+    <!-- Game One Card  -->
+    <div class="col-xl-3 col-md-3 mb-4">
+        <div class="card border-left-primary shadow h-100 py-2">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    
+                  <a href="{{ route('games.ticktak') }}" class="btn btn-success p-1">Tacktic tac toe</a>
+                </div>
+            </div>
+        </div>
     </div>
+
+  </div>
+</div>
 
   @endsection
